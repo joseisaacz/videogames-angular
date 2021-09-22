115135609030688e104b4b2feb3f40f9d2a6df2e
joseisaacz <joseisaaczj19@gmail.com>
2021-09-22 12:08:38 -0600

    keys

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -13,11 +13,10 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
     ): Observable<HttpEvent<any>> {
         req = req.clone({
             setHeaders:{
-                'x-rapidapi-key': 'ca3e18cb1emsh87fb1e4fa5e6ee5p145313jsn73b49a3ab376',
-                'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+              
             },
             setParams: {
-                key: 'f1b1bea22d7f4776a6a96c27f09db150'   
+               
             }
         });
         return next.handle(req);
